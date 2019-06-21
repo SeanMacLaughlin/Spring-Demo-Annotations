4a21b5a967b2dec99385d3948711fca4b15c5dfe
geneautry <geneautry@genes-mbp.home>
2019-06-20 23:48:30 -0400

    Checkpoint 75

--- spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -11,14 +11,13 @@ public class AnnotationDemoApp {
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// get the bean from spring container
-		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		Coach theCoach = context.getBean("pingPongCoach", Coach.class);
 		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		// call method to get the daily fortune
 		System.out.println(theCoach.getDailyFortune());
-		
 		
 		// close the context
 		context.close();
--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,17 +1,37 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class TennisCoach implements Coach {
 
+//	@Autowired
+//	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 	
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	/*
 	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
 		fortuneService = theFortuneService;
 	}
+	*/
+	
+//	@Autowired
+//	public TennisCoach(@Qualifier("randomFortuneService")
+//			FortuneService theFortuneService) {
+//		System.out.println(">> TennisCoach: inside constructor using @autowired and @qualifier");
+//		
+//		fortuneService = theFortuneService;
+//	}
+	
 	
 	@Override
 	public String getDailyWorkout() {
