c3306d383e6e12839aa5e80b4ac9a632b9572964
geneautry <geneautry@genes-mbp.home>
2019-06-21 17:28:36 -0400

    Checkpoint 83: Completed Spring configuration with annotations

--- spring-demo-annotations/src/com/luv2code/springdemo/PingPongCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/PingPongCoach.java
@@ -1,36 +1,36 @@
-package com.luv2code.springdemo;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-
-@Component
-public class PingPongCoach implements Coach {
-
-	
-	private FortuneService fortuneService;
-	
-	// define a default constructor
-	public PingPongCoach() {
-		System.out.println(">> PingPongCoach: inside default constructor");
-	}
-	
-	@Autowired
-	public PingPongCoach(@Qualifier("randomFortuneService")
-			FortuneService theFortuneService) {
-		System.out.println(">> PingPongCoach: inside constructor using @autowired and @qualifier");
-		
-		fortuneService = theFortuneService;
-	}
-		
-	@Override
-	public String getDailyWorkout() {
-		return "Practice your ping pong drop shot";
-	}
-
-	@Override
-	public String getDailyFortune() {
-		return fortuneService.getFortune();
-	}
-
-}
+//package com.luv2code.springdemo;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class PingPongCoach implements Coach {
+//
+//	
+//	private FortuneService fortuneService;
+//	
+//	// define a default constructor
+//	public PingPongCoach() {
+//		System.out.println(">> PingPongCoach: inside default constructor");
+//	}
+//	
+//	@Autowired
+//	public PingPongCoach(@Qualifier("randomFortuneService")
+//			FortuneService theFortuneService) {
+//		System.out.println(">> PingPongCoach: inside constructor using @autowired and @qualifier");
+//		
+//		fortuneService = theFortuneService;
+//	}
+//		
+//	@Override
+//	public String getDailyWorkout() {
+//		return "Practice your ping pong drop shot";
+//	}
+//
+//	@Override
+//	public String getDailyFortune() {
+//		return fortuneService.getFortune();
+//	}
+//
+//}
